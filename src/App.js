96f12ef46fcb77a8abe2b1d687852b5a2afb6eb3
Gabriel G <luisggm33@gmail.com>
2022-09-19 16:59:58 -0600

    switch test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+
+import { useEffect, useState, useRef } from 'react';
+import {createLocalTracks, connect} from 'twilio-video';
+import axios from 'axios'
+import qs from 'qs'
 import './App.css';
+const urlGenerateToken = 'https://generate-token-7936.twil.io/video'
+const identity = qs.stringify({identity:'yo'})
 
 function App() {
+
+  const [videoTracks, setVideoTracks] = useState({})
+  const [cameraMode, setCameraMode] = useState('user')
+  const ref = useRef(null)
+
+  useEffect( () => {
+
+    const initClient = async () => {
+      const token = await axios.post(urlGenerateToken, identity)
+
+      const tracks = await createLocalTracks({
+        audio: true,
+        video: {facingMode: 'user'}
+      })
+
+      setVideoTracks({tracks})
+
+      const room = await connect(token.data, {
+        name: "testRoom",
+        tracks
+      })
+
+      handleConnectedParticipant(room.localParticipant)
+      // room.participants.forEach(handleConnectedParticipant)
+      // room.on("participantConnected", handleConnectedParticipant)
+
+      // room.on("participantDisconnected", handleDisconnectedParticipant)
+      // window.addEventListener("pagehide", () => {room.disconnect()})
+      // window.addEventListener("beforeunload", () => {room.disconnect()})
+    }
+
+    initClient() 
+
+  }, [])
+
+  function handleConnectedParticipant(participant) {
+
+    participant.tracks.forEach((trackPublication) => {
+      handleTrackPublished(trackPublication, participant)
+    })
+    // participant.on("trackPublished", (trackPublication) => {
+    //   handleTrackPublished(trackPublication, participant)
+    // })
+
+  }
+
+  function handleTrackPublished(trackPublication, participant) {
+
+    function handleTrackSubscribed(track) {
+      
+
+      const el = ref.current
+      el.appendChild(track.attach())
+
+      // setVideoTracks(current => {
+      //   return({
+      //   ...current, 
+      //   [participant.sid]: current[participant.sid] ? [...current[participant.sid], track] : [track]
+      // })})
+
+
+      
+    }
+
+    if (trackPublication.track) {handleTrackSubscribed(trackPublication.track)}
+    // trackPublication.on("subscribed", handleTrackSubscribed)
+
+  }
+
+  // function handleDisconnectedParticipant(participant){
+  //   participant.removeAllListener()
+
+
+  // }
+
+  // useEffect(()=> {console.log({videoTracks})}, [videoTracks])
+
+  function swicthCamera() {
+    const mode = cameraMode === 'user' ? 'environment' : 'user'
+    const {tracks} = videoTracks
+    const cameraTrack = tracks.find(track => track.kind === 'video');
+    cameraTrack.restart({ facingMode: mode });
+    setCameraMode(cameraMode === 'user' ? 'environment' : 'user')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div ref={ref}></div> 
+     <button onClick={swicthCamera}>switch camera button</button>
     </div>
   );
 }
