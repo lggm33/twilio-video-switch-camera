5a2ba8b56b894b1657eafcee0fb925a6775a80d5
Gabriel G <luisggm33@gmail.com>
2022-09-20 14:34:04 -0600

    github

--- src/App.js
+++ src/App.js
@@ -5,52 +5,72 @@ import axios from 'axios'
 import qs from 'qs'
 import './App.css';
 const urlGenerateToken = 'https://generate-token-7936.twil.io/video'
-const identity = qs.stringify({identity:'yo'})
+const identity = qs.stringify('yp')
 
 function App() {
 
-  const [videoTracks, setVideoTracks] = useState({})
-  const [cameraMode, setCameraMode] = useState('user')
+  const [state, setState] = useState({
+    tracks: {}, 
+    cameraMode: 'user', 
+    room: null
+  })
+  const [inputValue, setInputValue] = useState("")
+
   const ref = useRef(null)
 
-  useEffect( () => {
+  
 
-    const initClient = async () => {
-      const token = await axios.post(urlGenerateToken, identity)
+  const initClient = async () => {
+    console.log(inputValue)
+    const identity = qs.stringify({identity: inputValue})
+    console.log({identity})
+    const token = await axios.post(urlGenerateToken, identity)
+    console.log({token})
+    const tracks = await createLocalTracks({
+      audio: false,
+      video: {facingMode: 'user'}
+    })
 
-      const tracks = await createLocalTracks({
-        audio: true,
-        video: {facingMode: 'user'}
-      })
 
-      setVideoTracks({tracks})
+    const room = await connect(token.data, {
+      name: "testRoom",
+      tracks
+    })
 
-      const room = await connect(token.data, {
-        name: "testRoom",
-        tracks
-      })
+    
 
-      handleConnectedParticipant(room.localParticipant)
-      // room.participants.forEach(handleConnectedParticipant)
-      // room.on("participantConnected", handleConnectedParticipant)
+    setState({...state, room, tracks: {[room.localParticipant.sid]: tracks}})
+    console.log({room})
 
-      // room.on("participantDisconnected", handleDisconnectedParticipant)
-      // window.addEventListener("pagehide", () => {room.disconnect()})
-      // window.addEventListener("beforeunload", () => {room.disconnect()})
-    }
+    handleConnectedParticipant(room.localParticipant)
+    room.participants.forEach(handleConnectedParticipant)
+    room.on("participantConnected", handleConnectedParticipant)
+   
+    room.on("participantDisconnected", handleDisconnectedParticipant)
+    window.addEventListener("pagehide", () => {room.disconnect()})
+    window.addEventListener("beforeunload", () => {room.disconnect()})
 
-    initClient() 
+    room.on('disconnected', room => {
+      // Detach the local media elements
+      room.localParticipant.tracks.forEach(publication => {
+        const attachedElements = publication.track.detach();
+        attachedElements.forEach(element => element.remove());
+      });
+    });
+  }
 
-  }, [])
+    
+
+  
 
   function handleConnectedParticipant(participant) {
-
+    console.log('handleConnectedParticipant')
     participant.tracks.forEach((trackPublication) => {
       handleTrackPublished(trackPublication, participant)
     })
-    // participant.on("trackPublished", (trackPublication) => {
-    //   handleTrackPublished(trackPublication, participant)
-    // })
+    participant.on("trackPublished", (trackPublication) => {
+      handleTrackPublished(trackPublication, participant)
+    })
 
   }
 
@@ -58,45 +78,70 @@ function App() {
 
     function handleTrackSubscribed(track) {
       
-
+      const trackElement = document.createElement('div')
+      trackElement.id = participant.sid
+      trackElement.appendChild(track.attach())
+      
       const el = ref.current
-      el.appendChild(track.attach())
+      el.appendChild(trackElement)
 
-      // setVideoTracks(current => {
-      //   return({
-      //   ...current, 
-      //   [participant.sid]: current[participant.sid] ? [...current[participant.sid], track] : [track]
-      // })})
+      setState(current => {
+        return({
+        ...current, 
+        [participant.sid]: current[participant.sid] ? [...current[participant.sid], track] : [track]
+      })})
 
 
       
     }
 
     if (trackPublication.track) {handleTrackSubscribed(trackPublication.track)}
-    // trackPublication.on("subscribed", handleTrackSubscribed)
+    trackPublication.on("subscribed", handleTrackSubscribed)
 
   }
 
-  // function handleDisconnectedParticipant(participant){
-  //   participant.removeAllListener()
+  function handleDisconnectedParticipant(participant){
+
+    console.log('handleDisconnectedParticipant')
+
+    participant.removeAllListeners()
+    const trackParticipant = document.getElementById(participant.sid)
+    trackParticipant.remove()
 
 
-  // }
+  }
 
-  // useEffect(()=> {console.log({videoTracks})}, [videoTracks])
+
+  useEffect(()=> {console.log({state})}, [state])
 
   function swicthCamera() {
-    const mode = cameraMode === 'user' ? 'environment' : 'user'
-    const {tracks} = videoTracks
-    const cameraTrack = tracks.find(track => track.kind === 'video');
+    const mode = state.cameraMode === 'user' ? 'environment' : 'user'
+    const localTracks = state.tracks[state.room.localParticipant.sid]
+    const cameraTrack = localTracks.find(track => track.kind === 'video');
     cameraTrack.restart({ facingMode: mode });
-    setCameraMode(cameraMode === 'user' ? 'environment' : 'user')
+    setState({...state, cameraMode: mode})
+  }
+
+  function handleInput(e) {
+    setInputValue(e.target.value)
+  }
+
+  function handleDisconnect() {
+    state.room.disconnect()
+    handleDisconnectedParticipant(state.room.localParticipant)
   }
 
   return (
     <div className="App">
      <div ref={ref}></div> 
+     <label>
+      Identity
+      <input type="text" value={inputValue} onChange={handleInput}></input>
+     </label>
+     <button onClick={initClient}>Join meeting</button>
      <button onClick={swicthCamera}>switch camera button</button>
+     <button onClick={handleDisconnect}>disconnect</button>
+     {state.room ? state.room.sid : "no yet"}
     </div>
   );
 }
